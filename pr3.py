cc6199e647326e7b29a2d2e3f28356445567a7eb
PaolaGT246 <a.23308051281187@cbtis128.edu.mx>
2024-11-22 21:10:31 -0600

    Add files via upload

---
+++ pr3.py
@@ -0,0 +1,9 @@
+print(" ")
+print("Gutierrez Torres Paola")
+print(" ")
+def filtrar_palabras(palabras, n):
+    return [palabra for palabra in palabras if len(palabra) > n]
+
+palabras = ["manzana", "sol", "elefante", "gato", "perro"]
+resultado = filtrar_palabras(palabras, 4)
+print(resultado) 
